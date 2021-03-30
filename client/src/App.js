9c213a451ba385b06e3c24d37be01f6ee8589cbe
imzhwch <imzhwch@gmail.com>
2021-03-30 14:49:02 +0700

    add suspensed edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,26 @@
-import { useSelector } from 'react-redux'
-import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom'
-import MainPage from './pages/MainPage'
-import NotFoundPage from './pages/404'
-import SignIn from './pages/Auth'
-import EditTask from './pages/EditTask'
+import { lazy, Suspense } from "react";
+import { useSelector } from "react-redux";
+import { Switch, Route, BrowserRouter, Redirect } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import NotFoundPage from "./pages/404";
+import SignIn from "./pages/Auth";
 
 function App() {
-  const { auth } = useSelector((store) => store.login)
+  const { auth } = useSelector((store) => store.login);
+  const EditTask = lazy(() => import("./pages/EditTask"));
+  const SuspensedEdit = () => (
+    <Suspense fallback={MainPage}>
+      <EditTask />
+    </Suspense>
+  );
+
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={MainPage} />
         <Route path="/login" component={SignIn} />
         {auth ? (
-          <Route path="/edit/:id" component={EditTask} />
+          <Route path="/edit/:id" component={SuspensedEdit} />
         ) : (
           <Redirect to="/">
             <Route component={MainPage} />
@@ -22,7 +29,7 @@ function App() {
         <Route component={NotFoundPage} />
       </Switch>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
